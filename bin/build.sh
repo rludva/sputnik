fcc54acd70df9ae951c83f226a90b7585ebd0827
Radomir Ludva <noreplay@radmi.cz>
2023-08-29 17:22:18 +0200

    Correct image tag version..

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@
 
 IMAGE_HOST="localhost"
 IMAGE_NAME="sputnik"
-IMAGE_TAG="1:03"
+IMAGE_TAG="1.03"
 
 DOCKERFILE="Dockerfile.ubi8"
 podman build -f "$DOCKERFILE" -t "$IMAGE_HOST"/"$IMAGE_NAME":"$IMAGE_TAG" .
